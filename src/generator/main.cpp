e2ab27b57485eb981b22ff693c4a841a97ad9177
Manuel Tomé <manuel.tome@epitech.eu>
2024-11-25 02:40:37 +0100

    feat: basic generator

--- src/generator/main.cpp
+++ src/generator/main.cpp
@@ -5,10 +5,29 @@
 ** main
 */
 
+#include "ArgParser.hpp"
+#include "FileHandler.hpp"
 #include <iostream>
 
-int main(void)
-{
-    std::cout << "Hello !\n";
+int main(int argc, char *argv[]) {
+  try {
+    auto args = ArgParser::parseGeneratorArgs(argc, argv);
+
+    for (const auto &[configFile, nbNetworks] : args.configs) {
+      std::string config = FileHandler::readFile(configFile);
+      for (int i = 1; i <= nbNetworks; i++) {
+        std::string outputFile =
+            configFile.substr(0, configFile.find_last_of('.')) + "_" +
+            std::to_string(i) + ".nn";
+
+        // TODO: Implement neural network generation
+        std::cout << "Generating network " << i << " from " << configFile
+                  << " to " << outputFile << std::endl;
+      }
+    }
     return 0;
+  } catch (const std::exception &e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+    return 84;
+  }
 }
