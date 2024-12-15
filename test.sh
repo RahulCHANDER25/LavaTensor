a3c1b640a87fd8682e2761521434a598f194d846
Manuel Tomé <manuel.tome@epitech.eu>
2024-12-15 16:12:39 +0100

    feat(training): New best NN 90%

--- test.sh
+++ test.sh
@@ -12,6 +12,6 @@ echo -e "\nGenerating neural network..."
 echo -e "\nTraining network..."
 ./my_torch_analyzer --train --save trained_network.nn best_network_1.nn examples/training_positions.txt
 
-# Test the network
-echo -e "\nTesting network..."
-./my_torch_analyzer --predict trained_network.nn examples/test_positions.txt
+## Test the network
+#echo -e "\nTesting network..."
+#./my_torch_analyzer --predict trained_network.nn examples/test_positions.txt
